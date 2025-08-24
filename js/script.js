d6ee7b3302f706039e068b6446180145a875600a
Niranjan <niranjan.veluthamessery@gmail.com>
2025-08-24 15:35:48 +0530

    Added popular movies

--- js/script.js
+++ js/script.js
@@ -2,6 +2,54 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+async function displayPopularMovies() {
+  const results = await fetchAPIData("movie/popular");
+
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+        <a href="movie-details.html?id=${movie.id}">
+          ${
+            movie.poster_path
+              ? `<img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.title}"
+          />`
+              : `
+          <img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+            alt="${movie.title}"
+          />`
+          }
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${movie.title}</h5>
+          <p class="card-text">
+            <small class="text-muted">Release: ${movie.release_date}</small>
+          </p>
+        </div>
+      `;
+    document.querySelector("#popular-movies").appendChild(div);
+  });
+}
+
+// Fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = CONFIG.TMDB_API_KEY;
+  const API_URL = "https://api.themoviedb.org/3";
+
+  const response = await fetch(
+    `${API_URL}/${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const { results } = await response.json();
+
+  return results;
+}
+
 // Highlight Active Link
 function highlightActiveLink() {
   const links = document.querySelectorAll(".nav-link");
@@ -11,13 +59,13 @@ function highlightActiveLink() {
     }
   });
 }
-// Init App
 
+// Init App
 function init() {
   switch (global.currentPage) {
     case "/":
     case "/index.html":
-      console.log("Home");
+      displayPopularMovies();
       break;
 
     case "/shows.html":
