2b9fe59fa0294254c7197ec4dc50be3c958b65f8
Niranjan VS <niranjan.veluthamessery@gmail.com>
2025-08-29 12:51:58 +0530

    Partially implementing search functionality

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 const global = {
   currentPage: window.location.pathname,
+  search: {
+    term: "",
+    type: "",
+    pages: 1,
+    totalPages: 1,
+  },
+  api: {
+    key: CONFIG.TMDB_API_KEY,
+    url: "https://api.themoviedb.org/3",
+  },
 };
 
 async function displayPopularMovies() {
@@ -192,6 +202,21 @@ function initSwiper() {
   });
 }
 
+// Search movies/shows
+
+async function search() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  global.search.type = urlParams.get("type");
+  global.search.term = urlParams.get("search-term");
+
+  if (global.search.term != "" && global.search.term != null) {
+    // TODO Make request and display search Results
+  } else {
+    showAlert("Please enter a search term!");
+  }
+}
+
 async function displayShowDetails() {
   const showId = window.location.search.split("=")[1];
   // console.log(`movie/${movieId}`);
@@ -289,10 +314,21 @@ function displayBackgroundImage(type, backgroundPath) {
   }
 }
 
+// Show Alert
+
+function showAlert(message, className) {
+  const alertEl = document.createElement("div");
+  alertEl.classList.add("alert", className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector("#alert").appendChild(alertEl);
+
+  setTimeout(() => alertEl.remove(), 3000);
+}
+
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
-  const API_KEY = CONFIG.TMDB_API_KEY;
-  const API_URL = "https://api.themoviedb.org/3";
+  const API_KEY = global.api.key;
+  const API_URL = global.api.url;
 
   showSpinner();
 
@@ -340,7 +376,7 @@ function init() {
       break;
 
     case "/search.html":
-      console.log("Search");
+      search();
       break;
   }
 
